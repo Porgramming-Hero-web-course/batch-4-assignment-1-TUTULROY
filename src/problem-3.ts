04489be9d2c0750638f65588eae5d9a46e2cb798
TUTULROY <tutulroyjoy@gmail.com>
2024-11-08 14:05:23 +0600

    problem 3  solved

---
+++ src/problem-3.ts
@@ -0,0 +1,13 @@
+
+function countWordOccurrences(sentence: string, word: string): number {
+  
+  const lowerSentence = sentence.toLowerCase();
+  const lowerWord = word.toLowerCase();
+
+  const words = lowerSentence.split(" ");
+
+  return words.filter(w => w === lowerWord).length;
+}
+
+
+console.log(countWordOccurrences("I love typescript", "love")); 
